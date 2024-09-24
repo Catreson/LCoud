b2020237a4c5bd65c9f3385f2b791385ad92c526
Catreson <arekdagiel@gmail.com>
2024-09-24 15:18:29 +0200

    Deletion and freeze

--- lcoud.py
+++ lcoud.py
@@ -4,18 +4,16 @@ import os
 import re
 
 
-passy = {}
-with open('pass.json', 'r') as file:
-    passy = json.load(file)
-os.environ["AWS_DEFAULT_REGION"] = 'eu-central-1'
-os.environ["AWS_ACCESS_KEY_ID"] = passy['AWS_ACCESS_KEY_ID']
-os.environ["AWS_SECRET_ACCESS_KEY"] = passy['AWS_SECRET_ACCESS_KEY']
+if os.path.isfile('pass.json'):
+    with open('pass.json', 'r') as file:
+        passy = json.load(file)
+    os.environ["AWS_DEFAULT_REGION"] = 'eu-central-1'
+    os.environ["AWS_ACCESS_KEY_ID"] = passy['AWS_ACCESS_KEY_ID']
+    os.environ["AWS_SECRET_ACCESS_KEY"] = passy['AWS_SECRET_ACCESS_KEY']
 
-def print_files(bucket, dir : str) -> None:
-    print(f'Content of bucket {bucket.name}/{dir}')
-    for obj in bucket.objects.filter(Prefix=dir):
-        print(obj.key)
-    print('Successfully printed content\n')
+if not (os.environ.get("AWS_ACCESS_KEY_ID") and os.environ.get("AWS_SECRET_ACCESS_KEY")):
+    raise Exception("Missing ENV")
+
 
 def upload_file(bucket, l_path : str, d_path : str) -> None:
     local_path = os.path.normpath(l_path)
@@ -33,13 +31,50 @@ def upload_file(bucket, l_path : str, d_path : str) -> None:
 
     print('Successfully uploaded file\n')
 
+
+def get_filtered_files(bucket, filter : str, dir : str) -> list:
+    return [obj for obj in bucket.objects.filter(Prefix=dir) if re.search(filter, obj.key)]
+
+
+def print_files(bucket, dir : str) -> None:
+    print(f'Content of bucket {bucket.name}/{dir}')
+    files = get_filtered_files(bucket, '', dir)
+    for obj in bucket.objects.filter(Prefix=dir):
+        print(obj.key)
+    print('Successfully printed content\n')
+
+
+def print_filtered_files(bucket, filter : str, dir : str) -> None:
+    print(f'Content of bucket {bucket.name}/{dir} filtered using {filter}')
+    files = get_filtered_files(bucket, filter, dir)
+    for file in files:
+        print(file.key)
+    print('Successfully printed filtered content\n')
+
+
+def delete_matching_files(bucket, filter : str, dir : str) -> None:
+    files = get_filtered_files(bucket, filter, dir)
+    decision = ''
+    for file in files:
+        decision = 'YES' if decision == 'YES' else input(f'Delete {file.key}? (y if yes, YES if yes for all):\t')
+        if decision == 'y' or decision == 'YES':
+            file.delete()
+    print('Successfully deleted filtered files\n')
+
+
 if __name__ == "__main__":
     s3 = boto3.resource(service_name='s3')
-    bucket = s3.Bucket(passy['bucket'])
+    bucket = s3.Bucket('developer-task')
     
     print_files(bucket, 'a-wing/')
 
-    local_path = input("Path to local file:\t")
-    dest_path = input("Destination:\t")
+    local_path = input('Path to local file:\t')
+    dest_path = input('Destination:\t')
     upload_file(bucket, local_path, dest_path)
+
+    re_filter = input('Provide regex filter for printing:\t')
+    print_filtered_files(bucket, re_filter, 'a-wing/')
+
+    re_filter = input('Provide regex filter for deletion:\t')
+    delete_matching_files(bucket, re_filter, 'a-wing/')
 
